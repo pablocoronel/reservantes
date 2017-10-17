b9602d9dee0891edcd6d987850c88234680bd847
Pablo Coronel <pablomanuelcoronel@gmail.com>
2017-10-17 16:15:10 -0300

    cambiado menu principal, agregada estructura de restaurante

---
+++ ReservAntes/Controllers/RestauranteController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReservAntes.Controllers
+{
+    public class RestauranteController : Controller
+    {
+        // GET: Restaurante
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Restaurante/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Restaurante/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurante/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Restaurante/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Restaurante/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Restaurante/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Restaurante/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
