2389cffd231587f92217517282bad5a75de57d45
PaliFerrara <pao.ferrara@live.com>
2018-02-12 19:46:55 -0300

    platoNuevoViewModel

---
+++ ReservAntes/ViewModels/PlatoElegidoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ReservAntes.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReservAntes.ViewModels
+{
+    [MetadataType(typeof(PlatosElegidosExtension))]
+    public class PlatoElegidoViewModel 
+    {
+        public bool elegido { get; set; }
+        public List<int> cantidadPlatos { get; set; }
+        public int cantidad { get; set; }
+    }
+}
