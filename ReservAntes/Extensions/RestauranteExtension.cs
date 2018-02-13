83568b2ea5e2bebbd5eebd592c7d724d9ed25e2c
Pablo Coronel <pablomanuelcoronel@gmail.com>
2018-02-13 02:17:30 -0300

    arreglado viewmodel de restaurante

--- ReservAntes/Extensions/RestauranteExtension.cs
+++ ReservAntes/Extensions/RestauranteExtension.cs
@@ -9,21 +9,38 @@ namespace ReservAntes.Extensions
     public class RestauranteExtension
     {
         public int IdRestaurante { get; set; }
+
         public int IdUsuario { get; set; }
+
+        //[Required(ErrorMessage = "Este campo es obligatorio")]
+        //[StringLength(50)]
+        //public string RazonSocial { get; set; }
+
         [Required(ErrorMessage = "Este campo es obligatorio")]
-        [StringLength(50)]
-        public string RazonSocial { get; set; }
         [StringLength(50)]
         public string NombreComercial { get; set; }
+
         public int? DatosBancariosId { get; set; }
+
         [Required(ErrorMessage = "Este campo es obligatorio")]
         public string CUIT { get; set; }
+
         public string Foto { get; set; }
+
         [Required(ErrorMessage = "Este campo es obligatorio")]
         public int CantidadClientes { get; set; }
+
         [Required(ErrorMessage = "Este campo es obligatorio")]
         public bool Habilitado { get; set; }
-        public int? DomicilioId { get; set; } 
+
+        //public int? DomicilioId { get; set; }
+
         public int? NivelId { get; set; }
+
+        public string Domicilio { get; set; }
+
+        public float Latitud { get; set; }
+
+        public float Longitud { get; set; }
     }
 }
